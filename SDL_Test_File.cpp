8b61dbaee88a3a1af39e2ed62f15473aa188b044
Cavan Theiss <cavanrt@gmail.com>
2025-11-25 13:14:45 -0600

    Basic window on screen for 5k nanoseconds

--- SDL_Test_File.cpp
+++ SDL_Test_File.cpp
@@ -3,6 +3,17 @@
 
 int main() {
 
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("Failed!");
+        return -1;
+    }
   
+    SDL_Window* window;
+    window = SDL_CreateWindow("Cavan - SDL3", 320, 240, SDL_WINDOW_RESIZABLE);
+
+    SDL_Delay(5000);
+
+    SDL_Quit();
+
     return 0;
 }
